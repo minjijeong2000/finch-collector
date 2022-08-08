9a627e42315ae82d1a964c8fb4b44cff5242fd1c
Minji <dudvnd13@gmail.com>
2022-08-07 22:12:42 -0700

    finish lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('finches/', views.finches_index, name="finches_index"),
     path('finches/<int:finch_id>/', views.finches_detail, name='finches_detail'),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('toys/', views.ToyList.as_view(), name='toys_index'),
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,21 @@ from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Finch, Toy
 from .forms import FeedingForm
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 
-class FinchCreate(CreateView):
+class Home(LoginView):
+  template_name = 'home.html'
+
+class FinchCreate(LoginRequiredMixin, CreateView):
     model = Finch
     fields = ['name', 'breed', 'description', 'age']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
     success_url = '/finches/'
     
 class FinchUpdate(UpdateView):
@@ -41,12 +52,13 @@ def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+@login_required
 def finches_index(request):
-    finches = Finch.objects.all()
+    finches = Finch.objects.filter(user=request.user)
     return render(request, 'finches/index.html', {'finches' : finches})
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values.list('id'))
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {'finch':finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 def add_feeding(request, finch_id):
@@ -59,3 +71,16 @@ def add_feeding(request, finch_id):
 def assoc_toy(request, finch_id, toy_id):
   Finch.objects.get(id=finch_id).toys.add(finch_id)
   return redirect('finches_detail', finch_id=finch_id)
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
